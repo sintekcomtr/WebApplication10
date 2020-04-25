6c9edc500137c837e766bdb37c4b2ac351a05737
alican <ekinci.alican@hotmail.com>
2020-04-25 14:40:54 +0300

    asd

---
+++ WebApplication10/Controllers/bosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication10.Controllers
+{
+    public class bosController : Controller
+    {
+        // GET: bos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
